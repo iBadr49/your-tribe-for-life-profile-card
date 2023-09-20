9bf9b4e03927d6676ac7d16e9b1ea4a82e047a61
iBadr49 <badr.amara@hva.nl>
2023-09-20 12:20:38 +0200

    Done
    ik had wat extra code verwijderd van de global.css file

--- p-card/src/routes/+page.svelte
+++ p-card/src/routes/+page.svelte
@@ -10,11 +10,10 @@
 
 <section class="profile-card">
 	<div class="image">
-		<img src={data.slices[0].primary.image.url} style="margin-right: 10px;" />
+		<img src={data.slices[0].primary.image.url} />
 	</div>
 	<div class="text">
 		<h1>{data.slices[0].primary.naam[0].text}</h1>
-
 		<p>{data.slices[0].primary.text[0].text}</p>
 		<p>{data.slices[0].primary.text2[0].text}</p>
 		<p id="email">{data.slices[0].primary.text3[0].text}</p>
@@ -22,14 +21,16 @@
 </section>
 
 <style>
-	img {
-		height: 200px;
-		width: auto;
-	}
-
 	.profile-card {
 		display: flex;
 		align-items: center;
+		margin: 50px;
+		padding: 20px;
+	}
+
+	img {
+		height: 200px;
+		width: auto;
 	}
 
 	.image {
